e9ddc27c9b2069269e91e4d3e430b019afc2e1ff
Matheus Gomes <matheusgomesteles@gmail.com>
2016-07-18 19:03:25 -0300

    Alteração na classe AgenteCliente

--- Imobiliaria/src/agentes/AgenteCliente.java
+++ Imobiliaria/src/agentes/AgenteCliente.java
@@ -5,6 +5,7 @@
  */
 package agentes;
 
+import jade.core.AID;
 import jade.core.Agent;
 import java.util.Iterator;
 
@@ -13,6 +14,10 @@ import java.util.Iterator;
  * @author Matheus
  */
 public class AgenteCliente extends Agent{
+    private Imovel imovel = new Imovel();
+    
+    private AID[] agentesProprietarios = {new AID("proprietario1", AID.ISLOCALNAME),
+                                          new AID("proprietario1", AID.ISLOCALNAME)};
     @Override
     protected void setup(){
         System.out.println("Oi, eu sou o agente cliente!");
@@ -23,5 +28,26 @@ public class AgenteCliente extends Agent{
         while(it.hasNext()){
             System.out.println("- " +it.next());
         }
+        
+        Object[] args = getArguments();
+        if(args != null && args.length > 0){
+            imovel.setTipoImovel((String) args[0]);
+            //imovel.setTamanho((double) args[1]);
+            //imovel.setQuantQuartos((int) args[2]);
+            //imovel.setLocalizacao((String) args[3]);
+            //imovel.setValor((double) args[4]);
+            
+            System.out.println("Procurando um(a)" + imovel.getTipoImovel() + " de " + imovel.getTamanho() +
+                                " metros quadrados, " + imovel.getQuantQuartos() + " quartos, no setor "
+                                + imovel.getLocalizacao() + ", com o valor de " + imovel.getValor());
+        }
+        else {
+            System.out.println("Sem parâmetros especificados");
+            doDelete();
+        }
+    }
+    
+    protected void takeDown(){
+        System.out.println("Agente cliente " + getAID().getName()+ " encerrando.");
     }
 }
